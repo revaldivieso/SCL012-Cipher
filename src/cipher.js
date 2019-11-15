a018ac0204fcbd38836783dfb6592dabd7d49b60
reyesvi92@gmail.com <reyesvi92@gmail.com>
2019-11-15 13:47:42 -0300

    cifrando minusculas en cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,53 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (palabras, chooseOffset) => {
+    let resultado = "";
+    // almacena el valor del string cifrado
+    for (let i = 0; i < palabras.length; i++) {
+      palabras.charCodeAt(i);
+      const letra = palabras.charCodeAt(i);
+      //representa el numero de la letra en el codigo ASCII
+      console.log(letra);
+
+      if (letra >= 65 && letra <= 90) {
+        //valor unicode de letras mayusculas en ASCII
+        let codAsciiCifrado = (letra - 65 + chooseOffset) % 26 + 65;
+        //formula de cifrado cesar: obtener nuevo numero de letra en el codigo ASCII
+        nuevoAscii = String.fromCharCode(codAsciiCifrado)
+        //obtener el valor de la letra cifrada
+        resultado += nuevoAscii; //formar el string cifrado 
+        console.log(resultado)
+      } else if (letra >= 97 && letra <= 122) {
+        //vaLor unicod de letras minusculas en ASCII
+        codAsciiCifrado = (letra - 97 + chooseOffset) % 26 + 97;
+        nuevoAscii = String.fromCharCode(codAsciiCifrado)
+        resultado += nuevoAscii;
+      } else {
+        resultado += String.fromCharCode(letra);
+        //retorna el valor plano
+      }
+    }
+    return resultado; // retorna el valor de la letra cifrada  
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (palabras, chooseOffset) => {
+    let resDecipher = "";
+    for (let i = 0; i < palabras.length; i++) {
+      palabras.charCodeAt(i);
+      const letra = palabras.charCodeAt(i);
+      console.log(resDecipher);
+      if (letra >= 65 && letra <= 90) {
+        let codAsciiDecifrado = (letra + 65 - chooseOffset) % 26 + 65; // formula para descifrar mayusculas
+        nuevoAscii = String.fromCharCode(codAsciiDecifrado); // obtener el valor de la letra descifrada
+        resDecipher += nuevoAscii; // formar la cadena descifrada     
+      } else if (letra >= 97 && letra <= 122) {
+        codAsciiDecifrado = (letra + 97 - chooseOffset) % 26 + 97; // descifra minusculas
+        nuevoAscii = String.fromCharCode(codAsciiDecifrado);
+        resDecipher += nuevoAscii;
+      } else {
+        resDecipher += String.fromCharCode(letra);
+        //retorna el valor plano
+      }
+    }
+    return resDecipher; // retorna el valor de la letra decifrada 
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-/*Acá va tu código*/
+const btnCifra = document.getElementById("cifrar");
+
+btnCifra.addEventListener("click",()=> {
+    const letras = document.getElementById("txt").value;
+    const chooseNumber = document.getElementById("offset").value;
+    console.log(letras);
+    const chooseOffset = parseInt(chooseNumber);
+    const res = window.cipher.encode(letras, chooseOffset);
+    const cifrado = document.getElementById("parrafo");
+    cifrado.innerHTML = res; 
+    //propiedad para acceder al html y le cambia el valor interno
+});
+
+const btnDecifra = document.getElementById("descifrar");
+
+btnDecifra.addEventListener("click",() => {
+    const letras = document.getElementById("txt").value;
+    const chooseNumber = document.getElementById("offset").value;
+    //const letrasTransformadas = letras.toUpperCase();
+    console.log(letras);
+    const chooseOffset = parseInt(chooseNumber);
+    const decipher = window.cipher.decode(letras, chooseOffset);
+    const textFinal = document.getElementById("parrafo");
+    textFinal.innerHTML = decipher; 
+});
+
+const btnReset = document.getElementById("resetear");
+btnReset.addEventListener("click",() => {
+    const letras = document.getElementById("txt");
+    console.log(letras);
+    letras.value= " "; 
+    const textFinal = document.getElementById("parrafo").innerHTML = " ";
+    console.log(textFinal);
+});
